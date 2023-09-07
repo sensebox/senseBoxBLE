79f2c52d9d346548109a7f831270ecd4e4db693c
Felix Erdmann <hello@felixerdmann.com>
2023-09-07 19:53:19 +0200

    chore: cleanup

--- src/SenseBoxBLE.cpp
+++ src/SenseBoxBLE.cpp
@@ -1,53 +1,17 @@
 #if defined(ARDUINO_SAMD_MKR1000)
 
-#include "NINAB31serial.h"
 #include "Arduino.h"
 #include <stdio.h>
 #include "SenseBoxBLE.h"
-/*
-BLEService SenseBoxBLE::phyphoxExperimentService{SenseBoxBLEExperimentServiceUUID}; // create service
-BLECharacteristic SenseBoxBLE::experimentCharacteristic{SenseBoxBLEExperimentCharacteristicUUID, BLERead | BLEWrite| BLENotify, 20, false};
-BLECharacteristic SenseBoxBLE::controlCharacteristic{SenseBoxBLEExperimentControlCharacteristicUUID, BLERead | BLEWrite| BLENotify, 20, false};
 
-BLEService SenseBoxBLE::phyphoxDataService{SenseBoxBLEDataServiceUUID}; // create service
-BLECharacteristic SenseBoxBLE::dataCharacteristic{SenseBoxBLEDataCharacteristicUUID, BLERead | BLEWrite | BLENotify, 20, false};
-BLECharacteristic SenseBoxBLE::configCharacteristic{SenseBoxBLEConfigCharacteristicUUID, BLERead | BLEWrite| BLENotify, 20, false};
-*/
 uint16_t SenseBoxBLE::minConInterval = 12;  //7.5ms
 uint16_t SenseBoxBLE::maxConInterval = 48; //30ms
 uint16_t SenseBoxBLE::slaveLatency = 0;
 uint16_t SenseBoxBLE::timeout = 50;
 
-uint16_t SenseBoxBLE::MTU = 20;
-// uint16_t SenseBoxBLEExperiment::MTU = 20;
-
-int SenseBoxBLE::h_service=0;
-int SenseBoxBLE::h_characteristic=0;
-
 uint8_t* SenseBoxBLE::data = nullptr; //this pointer points to the data the user wants to write in the characteristic
-uint8_t* SenseBoxBLE::p_exp = nullptr; //this pointer will point to the byte array which holds an experiment
-
-size_t SenseBoxBLE::expLen = 0; //try o avoid this maybe use std::array or std::vector
-uint8_t SenseBoxBLE::EXPARRAY[4000] = {0};// block some storage
-
-uint8_t SenseBoxBLE::controlCharValue[21]={0};
-uint8_t SenseBoxBLE::configCharValue[21]={0};
-
-
 
 void(*SenseBoxBLE::configHandler)() = nullptr;
-
-void SenseBoxBLE::start(const char* DEVICE_NAME, uint8_t* exp_pointer, size_t len){
-  p_exp = exp_pointer;
-  expLen = len;
-  start(DEVICE_NAME);
-}
-
-void SenseBoxBLE::start(uint8_t* exp_pointer, size_t len){
-  p_exp = exp_pointer;
-  expLen = len;
-  start();
-}
 
 void SenseBoxBLE::start(const char* DEVICE_NAME)
 {
@@ -97,37 +61,16 @@ void SenseBoxBLE::poll(int timeout)
 
 void SenseBoxBLE::poll(){
     if(port.poll()){
-        // if(parseValue(controlCharValue,port.checkCharWritten(h_controlCharacteristic))){
-        //     controlCharacteristicWritten();
-        // }
-        // if(parseValue(configCharValue,port.checkCharWritten(h_configCharacteristic))){
-        //     configCharacteristicWritten();
-        // }
         port.flushInput();
     }
 
 }
-
-void SenseBoxBLE::read(uint8_t *arrayPointer, unsigned int arraySize)
-{
-  memcpy(arrayPointer, configCharValue, arraySize);
-  //configCharacteristic.readValue(arrayPointer, arraySize);
-}
-
-void SenseBoxBLE::read(float& f)
-{
-  //uint8_t readDATA[4];
-  //configCharacteristic.readValue(readDATA, 4);
-  memcpy(&f,configCharValue,4);
-}
-
 
 bool SenseBoxBLE::write(int characteristic, float& value)
 {
   data = reinterpret_cast<uint8_t*>(&value);
   port.writeValue(characteristic,data,4);
 }
-
 
 bool SenseBoxBLE::write(int characteristic, float& f1, float& f2)
 {
@@ -157,22 +100,4 @@ bool SenseBoxBLE::write(int characteristic, float& f1, float& f2, float& f3 , fl
   return port.writeValue(characteristic,data,20);
 }
 
-
-bool parseValue(uint8_t* target, String s)
-{
-    String hextable="0123456789ABCDEF";
-    if(s.length() && s.length()<41){
-        s.toUpperCase();
-        for(int i=0;i<s.length();i+=2){
-            int highbits=hextable.indexOf(s[i]);
-            int lowbits=hextable.indexOf(s[i+1]);
-            if(highbits==-1 || lowbits==-1){
-                return false;
-            }
-            target[i]=(((highbits<<4)|lowbits)&0xff);
-        }
-        return true;
-    }
-    return false;
-}
 #endif
--- src/SenseBoxBLE.h
+++ src/SenseBoxBLE.h
@@ -8,20 +8,8 @@ class SenseBoxBLE
 
         static NINAB31Serial port;
 
-        static void controlCharacteristicWritten();
-        static void configCharacteristicWritten();
-
-        static int h_service;
-        static int h_characteristic;
-
         static uint8_t* data; //this pointer points to the data the user wants to write in the characteristic
-        static uint8_t* p_exp; //this pointer will point to the byte array which holds an experiment
 
-        static size_t expLen; //try o avoid this maybe use std::array or std::vector
-        static uint8_t EXPARRAY[4000];// block some storage
-
-        static uint8_t controlCharValue[21];
-        static uint8_t configCharValue[21];
 
     public:
 
@@ -42,9 +30,6 @@ class SenseBoxBLE
         static bool write(int, float&, float&, float&, float&);
         static bool write(int, float&, float&, float&, float&, float&);
 
-        static void read(uint8_t*, unsigned int);
-        static void read(float&);
-
         static void poll();
         static void poll(int timeout);
 
@@ -53,6 +38,5 @@ class SenseBoxBLE
         static uint16_t maxConInterval;
         static uint16_t slaveLatency;
         static uint16_t timeout;
-        static uint16_t MTU;
 
 };
